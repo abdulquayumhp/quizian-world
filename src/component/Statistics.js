33d9a9bb2f46a443de99504399efe2ad9c0bd9b8
abdulquayumhp <badulquayumhp998@gmailc.com>
2022-10-11 19:56:45 +0600

    success and fail toast add

--- src/component/Statistics.js
+++ src/component/Statistics.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Line, LineChart, Tooltip, XAxis, YAxis } from "recharts";
+import { QuizData } from "./Root";
 
 const Statistics = () => {
+	const quiz = useContext(QuizData);
+	console.log(quiz);
+	const { total } = quiz[0];
+	console.log(total);
+
 	return (
 		<div>
-			<h1>Statistic</h1>
+			<LineChart width={500} height={400} data={quiz}>
+				<Line type="monotone" dataKey="total" stroke="#82ca9d" />
+				<XAxis dataKey="name" />
+				<YAxis />
+				<Tooltip />
+			</LineChart>
 		</div>
 	);
 };
